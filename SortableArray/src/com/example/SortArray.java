8e5f3d40d8aba6536da2890e8ecbb31c6fc8b183
IshuKant <Ishu.kant@student.kpu.ca>
2024-11-14 14:09:42 +0530

    added-srt-integer-feature

---
+++ SortableArray/src/com/example/SortArray.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class SortArray {
+    public static int[] integersSort(int[] numbers) {
+        Arrays.sort(numbers);
+        return numbers;
+    }
+  
+}
